c43ceca6710dc85824a8a8c4b14c6092e44881c8
ryankklam <ryan4299899@126.com>
2019-12-24 13:10:21 +0800

    rewrite use array for track depth. This can solve all testcases

--- tutorial/src/main/java/Algorithms/BFSShortReach.java
+++ tutorial/src/main/java/Algorithms/BFSShortReach.java
@@ -6,6 +6,10 @@ import java.util.*;
  * https://www.hackerrank.com/challenges/bfsshortreach/problem
  */
 public class BFSShortReach {
+    
+    static LinkedList[] adjency;
+    static int[] depth;
+    static boolean[] visited;
 
     // Complete the bfs function below.
     static int[] bfs(int n, int m, int[][] edges, int s) {
@@ -15,63 +19,119 @@ public class BFSShortReach {
         //edges: a 2D array of start and end nodes for edges
         //s: the node to start traversals from
 
-        Map<Integer,List> graph = new HashMap<Integer,List>();
+        //initial adjency map
+        depth = new int[n+1];
+        visited = new boolean[n+1];
+        adjency = new LinkedList[n+1];
+        for(int i=0;i<=n;i++){
+            adjency[i] = new LinkedList<Integer>();
+        }
 
+        //fill edges to node adjency map
         for(int i=0;i<edges.length;i++){
             int from = edges[i][0];
             int to = edges[i][1];
 
-            if(graph.get(Integer.valueOf(from))==null){
-                List temp  = new ArrayList();
-                temp.add(to);
-                graph.put(from,temp);
-            }else {
-                graph.get(Integer.valueOf(from)).add(to);
-            }
+            adjency[from].add(to);
+            adjency[to].add(from);
         }
 
-        LinkedList graphList = new LinkedList();
-        graphList.offer(s);
+        //
+        Queue nodeQueue = new LinkedList<>();
+        nodeQueue.add(s);
+        visited[s] = true;
+        depth[s] = 0;
 
-        int[] result= new int[n];
+        while(!nodeQueue.isEmpty()){
+           int parent = (int) nodeQueue.poll();
+           int parentSize = adjency[parent].size();
+           for(int i=0;i<parentSize;i++){//can't use adjency[parent].size() for loop as each time poll the size got changed
+               int node = (int) adjency[parent].poll();
 
-        Set checkedSet = new HashSet();
-        boolean nextLevel = true;
-        LinkedList nextLevelList = new LinkedList();
-        nextLevelList.offer(s);
-
-        int step=-1;
-        while(!graphList.isEmpty()){
-            step++;
-            int node = (int)graphList.poll();
-
-            //skip checked node
-            if(checkedSet.contains(node)) continue;
-
-
-
-            addNodesToList(graphList,graph.getOrDefault(node,new ArrayList()));
-
-            result[node-1] = step*6;
-
-            checkedSet.add(node);
+               if(!visited[node]){
+                   depth[node] =   depth[parent] +6;
+                   visited[node] = true;
+                   nodeQueue.add(node);
+               }
+           }
         }
 
         //rearrange the last result , remove 0 , and s element
         int[] finalResult = new int[n-1];
-        for(int i=0;i<n;i++){
-            if(i>s){
-                finalResult[i-1] = result[i];
+        for(int i=0;i<n-1;i++){
+            if(i>=s-1){
+                finalResult[i] = depth[i+2]==0 ? -1:depth[i+2];
             }else {
-                finalResult[i] = result[i];
+                finalResult[i] = depth[i+1]==0 ? -1:depth[i+1];
             }
         }
         return finalResult;
-    }
 
-    public static void addNodesToList(LinkedList graphList,List tempList){
-        for(int i=0;i<tempList.size();i++){
-            graphList.offer(tempList.get(i));//Adds the specified element as the tail (last element) of this list.
-        }
+
+//        Map<Integer,List> graph = new HashMap<Integer,List>();
+//
+//        for(int i=0;i<edges.length;i++){
+//            int from = edges[i][0];
+//            int to = edges[i][1];
+//
+//            if(graph.get(Integer.valueOf(from))==null) {
+//                List temp = new ArrayList();
+//                graph.put(from, temp);
+//            }
+//            graph.get(Integer.valueOf(from)).add(to);
+//
+//            if(graph.get(Integer.valueOf(to))==null) {
+//                List temp = new ArrayList();
+//                graph.put(to, temp);
+//            }
+//            graph.get(Integer.valueOf(to)).add(from);
+//        }
+//
+//        LinkedList graphList = new LinkedList();
+//        graphList.offer(s);
+//
+//        int[] result= new int[n];
+//
+//        Set checkedSet = new HashSet();
+//        boolean toNextLevel = true;
+//        LinkedList nextLevelList = new LinkedList();
+//
+//        int step=-1;
+//        while(!graphList.isEmpty()){
+//
+//            int node = (int)graphList.poll();
+//
+//            if(toNextLevel) {
+//                step++;
+//                toNextLevel=false;
+//            }
+//
+//            //skip checked node
+//            if(checkedSet.contains(node)) continue;
+//
+////            addNodesToList(graphList,graph.getOrDefault(node,new ArrayList()));
+//            nextLevelList.addAll(graph.getOrDefault(node,new ArrayList()));
+//
+//            result[node-1] = step*6 ==0 ? -1: step*6;
+//
+//            checkedSet.add(node);
+//
+//            if(graphList.isEmpty()){
+//                graphList.addAll(nextLevelList);
+//                nextLevelList.clear();
+//                toNextLevel = true;
+//            }
+//        }
+//
+//        //rearrange the last result , remove 0 , and s element
+//        int[] finalResult = new int[n-1];
+//        for(int i=0;i<finalResult.length;i++){
+//            if(i>=s-1){
+//                finalResult[i] = result[i+1]==0 ? -1:result[i+1];
+//            }else {
+//                finalResult[i] = result[i]==0 ? -1:result[i];
+//            }
+//        }
+//        return finalResult;
     }
 }
--- tutorial/src/test/java/Algorithms/BFSShortReachTest.java
+++ tutorial/src/test/java/Algorithms/BFSShortReachTest.java
@@ -17,4 +17,33 @@ public class BFSShortReachTest {
         assertArrayEquals(result,BFSShortReach.bfs(4,2,edges,1));
     }
 
+    @Test
+    public void test2(){
+
+        int[][] edges = new int[][]{{2,3}};
+
+        int[] result = new int[]{-1,6};
+        assertArrayEquals(result,BFSShortReach.bfs(3,1,edges,2));
+    }
+
+    @Test
+    public void test3(){
+
+        int[][] edges = new int[][]{{1,2},{2,3},{2,5},{3,4}};
+
+        int[] result = new int[]{6,12,18,12};
+        assertArrayEquals(result,BFSShortReach.bfs(5,4,edges,1));
+    }
+
+
+
+    @Test
+    public void test5(){
+
+        int[][] edges = new int[][]{{1,2},{1,3},{3,4}};
+
+        int[] result = new int[]{6,6,12,-1};
+        assertArrayEquals(result,BFSShortReach.bfs(5,3,edges,1));
+    }
+
 }
