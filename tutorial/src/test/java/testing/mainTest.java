e46afa3c562ee862450f922814e45524dac599b5
RyanKKLam <ryan4299899@126.com>
2020-12-09 02:12:18 +0800

    json combine

--- tutorial/src/test/java/testing/mainTest.java
+++ tutorial/src/test/java/testing/mainTest.java
@@ -1,8 +1,62 @@
 package testing;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class mainTest {
+
+    @Test
+    public void jsonTest() throws Exception{
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        //test  using json string
+        String json = "{\"2\":\"efg\",\"1\":\"abc\"}";
+        String json2 = "{\"3\":\"ppp\",\"4\":\"xxx\"}";
+
+
+        //test using object
+        // create three JSON objects
+        ObjectNode user1 = mapper.createObjectNode();
+        user1.put("id", 1);
+        user1.put("name", "John Doe");
+
+        ObjectNode user2 = mapper.createObjectNode();
+        user2.put("id", 2);
+        user2.put("name", "Tom Doe");
+
+        ObjectNode user3 = mapper.createObjectNode();
+        user3.put("id", 3);
+        user3.put("name", "Emma Doe");
+
+
+
+        // create `ArrayNode` object
+        ArrayNode jsonStringArrayNode = mapper.createArrayNode();
+        jsonStringArrayNode.add(json);
+        jsonStringArrayNode.add(json2);
+
+
+        // add JSON users to array
+        ArrayNode objectArrayNode = new ObjectMapper().createArrayNode();
+        objectArrayNode.addAll(Arrays.asList(user1, user2, user3));
+
+        // convert `ArrayNode` to pretty-print JSON
+        // without pretty-print, use `arrayNode.toString()` method
+        String jsonStrCombine = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonStringArrayNode);
+        String objectStrCombine = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(objectArrayNode);
+
+        System.out.println(jsonStrCombine);
+        System.out.println(objectStrCombine);
+
+
+//
+//        JSONObject json_test = JSONObject.fromObject(json);
+    }
 
     @Test
     public void demoTest(){
